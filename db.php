434fcc847880773d23cb7a5fc5cf5969d1ee7159
Lucas Gomes <lulinux2109@gmail.com>
2025-07-10 02:14:02 -0300

    feat: add config db

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$db = 'agenda';
-$user = 'root';
-$pass = '';
+$host = 'dpg-d1nkidripnbc73ap70p0-a';
+$db = 'agenda_db_c1j1';
+$user = 'agenda_db_c1j1_user';
+$pass = 'keDH88bCykCksPR7WTZxCaY4ViuL7O0j';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
