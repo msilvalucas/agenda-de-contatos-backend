4db9d42cc19af88971d504b52399e733b4557469
Lucas Gomes <lulinux2109@gmail.com>
2025-07-10 02:24:53 -0300

    feat: add config db

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
-$host = 'dpg-d1nkidripnbc73ap70p0-a';
-$db = 'agenda_db_c1j1';
-$user = 'agenda_db_c1j1_user';
-$pass = 'keDH88bCykCksPR7WTZxCaY4ViuL7O0j';
+$host = 'caboose.proxy.rlwy.net';
+$port = 52709;
+$db = 'railway';
+$user = 'root';
+$pass = 'PZzcvtDjfSLQQpChNyhTllsczQvSdIg';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     http_response_code(500);
