0f471af6e03eb5bcbbbc639647d8a7ca4f2f0c98
Lucas Gomes <lulinux2109@gmail.com>
2025-07-10 03:12:39 -0300

    refactor: cors

--- config/headers.php
+++ config/headers.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://minha-loja-front.infinityfreeapp.com");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
